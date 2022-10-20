b9e3606d1ea664ff00b0bb9e2fff2fe175937a35
bigchunguspng <70759405+bigchunguspng@users.noreply.github.com>
2022-10-20 22:11:49 +0300

    #

--- Witlesss/Bot.cs
+++ Witlesss/Bot.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.IO;
 using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Extensions.Polling;

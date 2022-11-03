591b7c2e6b0d5f37bb19a242e9c7232ca17c682c
bigchunguspng <70759405+bigchunguspng@users.noreply.github.com>
2022-11-03 23:09:03 +0200

    ##

--- Witlesss/Handler.cs
+++ Witlesss/Handler.cs
@@ -4,7 +4,6 @@ using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
-using Telegram.Bot.Types.Enums;
 using Witlesss.Commands;
 using static Witlesss.Extension;
 using static Witlesss.Logger;
@@ -13,36 +12,36 @@ namespace Witlesss
 {
     public class Handler : IUpdateHandler
     {
-        private readonly MainJunction _fork;
-            
-        public Handler() => _fork = new MainJunction();
+        private readonly MainJunction _fork = new();
 
         public Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
         {
-            if      (update.Type == UpdateType.Message)
-                TryHandleMessage(update.Message);
-            else if (update.Type == UpdateType.EditedMessage)
-                TryHandleMessage(update.EditedMessage);
-            return Task.CompletedTask;
+            return update switch
+            {
+                { Message:       { } message } => OnMessage(message),
+                { EditedMessage: { } message } => OnMessage(message)
+            };
         }
 
         public Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
         {
-            LogError("Telegram API Error x_x --> " + exception.Message);
+            LogError($"Telegram API Error x_x --> {exception.Message}");
             return Task.CompletedTask;
         }
         
-        private void TryHandleMessage(Message message)
+        private Task OnMessage(Message message)
         {
             try
             {
                 _fork.Pass(message);
                 _fork.Run();
             }
-            catch (Exception exception)
+            catch (Exception e)
             {
-                LogError(TitleOrUsername(message) + " >> Can't process message --> " + exception.Message);
+                LogError($"{TitleOrUsername(message)} >> Can't process message --> {e.Message}");
             }
+
+            return Task.CompletedTask;
         }
     }
 }

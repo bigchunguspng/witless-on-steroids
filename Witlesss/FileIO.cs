f2c9e212c313dabee3923590eacd18023013a6d9
bigchunguspng <70759405+bigchunguspng@users.noreply.github.com>
2023-02-14 21:59:54 +0200

    FileIO.cs if it was an efficient tool

--- Witlesss/FileIO.cs
+++ Witlesss/FileIO.cs
@@ -1,43 +1,44 @@
 ﻿using Newtonsoft.Json;
-using static Newtonsoft.Json.JsonConvert;
 
 namespace Witlesss
 {
-    public class FileIO<T> where T : new()
+    public class FileIO<T> : FileIO_Static where T : new()
     {
         private readonly string _path;
-        private readonly JsonSerializerSettings _settings;
 
-        public FileIO(string path)
-        {
-            _path = path;
-            _settings = new JsonSerializerSettings
-            {
-                Formatting = Formatting.Indented, 
-                DefaultValueHandling = DefaultValueHandling.Ignore
-            };
-        }
+        public FileIO(string path) => _path = path;
 
         public T LoadData()
         {
-            if (FileEmptyOrNotExist(_path))
-            {
-                CreateFilePath(_path);
-                File.CreateText(_path).Dispose();
-                T result = new();
-                SaveData(result);
-                return result;
-            }
+            if (FileEmptyOrNotExist(_path)) return NewT();
 
             using var stream = File.OpenText(_path);
             using var reader = new JsonTextReader(stream);
-            return new JsonSerializer().Deserialize<T>(reader);
+            return Serializer.Deserialize<T>(reader);
         }
 
         public void SaveData(T db)
         {
-            using var writer = File.CreateText(_path);
-            writer.Write(SerializeObject(db, _settings));
+            using var stream = File.CreateText(_path);
+            using var writer = new JsonTextWriter(stream);
+            Serializer.Serialize(writer, db);
         }
+
+        private T NewT()
+        {
+            CreateFilePath(_path);
+            T result = new();
+            SaveData(result);
+            return result;
+        }
+    }
+
+    public class FileIO_Static
+    {
+        protected static readonly JsonSerializer Serializer = new()
+        {
+            Formatting = Formatting.Indented,
+            DefaultValueHandling = DefaultValueHandling.Ignore
+        };
     }
 }

a473e634b3bbc426871686c1b988994b08b97af6
bigchunguspng <70759405+bigchunguspng@users.noreply.github.com>
2023-02-16 21:27:17 +0200

    ###

--- Witlesss/MemeGenerator.cs
+++ Witlesss/MemeGenerator.cs
@@ -17,15 +17,16 @@ namespace Witlesss
         private bool _resize;
         private const int _m = 10;
         private readonly FontFamily    _font = new("Impact");
+        private readonly SolidBrush   _white = new(Color.White);
         private readonly StringFormat _upper = new() { Alignment = Center, Trimming = Word };
         private readonly StringFormat _lower = new() { Alignment = Center, Trimming = Word, LineAlignment = Far};
-        private readonly Dictionary<ColorMode, PaintBrush> _brushes;
+        private readonly Dictionary<ColorMode, Func<SolidBrush>> _brushes;
         private static readonly Regex Ext = new("(.png)|(.jpg)");
 
-        public MemeGenerator() => _brushes = new Dictionary<ColorMode, PaintBrush>
+        public MemeGenerator() => _brushes = new Dictionary<ColorMode, Func<SolidBrush>>
         {
-            { ColorMode.Color, new RandomBrush(this) },
-            { ColorMode.White, new WhiteBrush() }
+            { ColorMode.Color, RandomColor },
+            { ColorMode.White, WhiteColor }
         };
 
         public void SetUp(Size size)
@@ -35,8 +36,6 @@ namespace Witlesss
             _s = Math.Max((int)Math.Min(_w, 1.5 * _h) / 12, 12);
             _d = _h / 3 * 2;    // upper margin for bottom text
         }
-
-        public int OutlineWidth => (int)Math.Round(_s / 6D);
 
         public string MakeImpactMeme(string path, DgText text)
         {
@@ -77,7 +76,9 @@ namespace Witlesss
             g.FillPath(Brush(), path);
         }
 
-        private SolidBrush Brush() => _brushes[WitlessCommand.Baka.Meme.Dye].Brush;
+        private int OutlineWidth => (int)Math.Round(_s / 6D);
+
+        private SolidBrush Brush() => _brushes[WitlessCommand.Baka.Meme.Dye].Invoke();
 
         private Image GetImage(string path)
         {
@@ -99,6 +100,49 @@ namespace Witlesss
             var crop = new Rectangle(0, 0, image.Width - 1, image.Height - 1);
             var bitmap = new Bitmap(image);
             return bitmap.Clone(crop, bitmap.PixelFormat);
+        }
+
+        private SolidBrush WhiteColor() => _white;
+        
+        private SolidBrush RandomColor()
+        {
+            var h = Extension.Random.Next(360);
+            var s = Extension.Random.NextDouble();
+            var v = Extension.Random.NextDouble();
+
+            var o = Math.Min(OutlineWidth,       6);
+            var x = Math.Min(Math.Abs(240 - h), 60);
+
+            s = s * (0.75 + x / 240D);  // <-- removes dark blue
+            s = s * (0.25 + 0.125 * o); // <-- makes small text brighter
+
+            v = 1 - 0.3 * v * Math.Sqrt(s);
+
+            return new SolidBrush(ColorFromHSV(h, s, v));
+        }
+
+        private static Color ColorFromHSV(double hue, double saturation, double value)
+        {
+            var sextants = hue / 60;
+            var triangle = Math.Floor(sextants);
+            int dye = Convert.ToInt32(triangle) % 6;
+            var fraction = sextants - triangle;
+
+            value = value * 255;
+            int v = Convert.ToInt32(value);
+            int p = Convert.ToInt32(value * (1 - saturation));
+            int q = Convert.ToInt32(value * (1 - saturation * fraction));
+            int t = Convert.ToInt32(value * (1 - saturation * (1 - fraction)));
+
+            return dye switch
+            {
+                0 => Color.FromArgb(v, t, p),
+                1 => Color.FromArgb(q, v, p),
+                2 => Color.FromArgb(p, v, t),
+                3 => Color.FromArgb(p, q, v),
+                4 => Color.FromArgb(t, p, v),
+                _ => Color.FromArgb(v, p, q)
+            };
         }
     }
 }

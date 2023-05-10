881b0140b7c2fa7c4cbf653537e3e7b1e5bec2a9
bigchungus <70759405+bigchunguspng@users.noreply.github.com>
2023-05-10 17:42:41 +0300

    /songnc

--- Witlesss/Commands/DownloadMusic.cs
+++ Witlesss/Commands/DownloadMusic.cs
@@ -15,6 +15,7 @@ namespace Witlesss.Commands
         private readonly Regex _name = new(              @"(?:NA - )?(?:([\S\s]+) - )?([\S\s]+)? xd\.mp3");
         private readonly Regex   _id = new(@"(?:(?:\?v=)|(?:v\/)|(?:\.be\/)|(?:embed\/)|(?:u\/1\/))([\w\d-]+)");
         private readonly Regex  _ops = new(@"\/song(\S*[qnpc]+)+");
+        private readonly Regex _bruh = new(@"(\s\(([\S\s][^\(]+)\))|(\s\[([\S\s][^\[]+)\])");
 
         private readonly string _url_prefix = "https://youtu.be/";
         
@@ -58,8 +59,8 @@ namespace Witlesss.Commands
             var xt = options.Contains('p'); // extract thumbnail (from video) (otherwise use youtube one)
             var rb = options.Contains('c'); // remove brackets
 
-            var audio = hq ? " --audio-quality 0" : "";
-            var thumb = xt ? "" : " --write-thumbnail";
+            var audio = hq ?      " --audio-quality 0" : "";
+            var thumb = xt ? "" : " --write-thumbnail -o \"thumbnail:thumb.%(ext)s\"";
 
             var cmd_a = $"/C yt-dlp --no-mtime -f 251 -k -x --audio-format mp3{audio}{thumb} {url} -o \"{artist ?? "%(artist)s"} - {title ?? "%(title)s"} xd\"";
             var cmd_v = xt ? $"/C yt-dlp --no-mtime -f \"bv*[height<=720][filesize<15M]\" -k {url} -o \"video xd.%(ext)s\"" : null;
@@ -71,12 +72,15 @@ namespace Witlesss.Commands
             if (xt) await RunCMD(cmd_v, dir);
 
             var di = new DirectoryInfo(dir);
-            var vid = di.GetFiles(xt ? "video xd.*" : "*xd.webp")[0].FullName; // video : thumb itself
-            var mp3 = di.GetFiles(                    "*xd.mp3" )[0].FullName;
+            var vid = di.GetFiles(xt ? "video xd.*" : "thumb.*")[0].FullName; // video : thumb itself
+            var mp3 = di.GetFiles(          "*xd.mp3"          )[0].FullName;
 
             var meta = _name.Match(Path.GetFileName(mp3));
             if (artist is null && meta.Groups[1].Success) artist = meta.Groups[1].Value;
             if (title  is null && meta.Groups[2].Success) title  = meta.Groups[2].Value;
+
+            if (no) artist = null;
+            if (rb) title = _bruh.Replace(title!, "");
 
             var ffmpeg = new F_Resize(vid);
             var art = xt ? ffmpeg.ExportThumbnail() : ffmpeg.Transcode(".png");
@@ -85,7 +89,7 @@ namespace Witlesss.Commands
 
             await using var stream = File.OpenRead(track);
             Bot.SendAudio(cp.Chat, new InputOnlineFile(stream, track), jpg);
-            Log($"{cp.Title} >> SONG [mp3]");
+            Log($"{cp.Title} >> YOUTUBE MUSIC [mp3]");
         }
 
         private static async Task RunCMD(string cmd, string directory)

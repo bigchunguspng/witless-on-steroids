ac4bebf21e00a6e62c1c9d2f1dad4bec65c41b35
bigchungus <70759405+bigchunguspng@users.noreply.github.com>
2023-06-20 22:42:24 +0300

    #soundclown

--- Witlesss/Commands/DownloadMusic.cs
+++ Witlesss/Commands/DownloadMusic.cs
@@ -42,7 +42,8 @@ namespace Witlesss.Commands
                 var artist = args.Groups[2].Success ? args.Groups[2].Value : null;
                 var title  = args.Groups[3].Success ? args.Groups[3].Value : null;
 
-                var id = _id.Match(url).Groups[1].Value;
+                var yt = url.Contains("youtu");
+                var id = yt ? _id.Match(url).Groups[1].Value : url.Contains('?') ? url[..url.IndexOf('?')] : url;
                 if (id.Length < 1) throw new Exception("video id was too small");
 
                 var ops = _ops.Match(RemoveBotMention());
@@ -50,7 +51,7 @@ namespace Witlesss.Commands
 
                 var message = Bot.PingChat(Chat, Pick(PLS_WAIT_RESPONSE));
 
-                RunSafelyAsync(DownloadSongAsync(id, artist, title, options, cover, message, SnapshotMessageData()), Chat, message);
+                RunSafelyAsync(DownloadSongAsync(id, artist, title, options, cover, message, yt, SnapshotMessageData()), Chat, message);
             }
             else
             {
@@ -60,7 +61,7 @@ namespace Witlesss.Commands
 
         private string GetPhotoFileID(Message message) => message?.Photo is { } p ? p[^1].FileId : null;
 
-        private async Task DownloadSongAsync(string id, string artist, string title, string options, string file, int message, CommandParams cp)
+        private async Task DownloadSongAsync(string id, string artist, string title, string options, string file, int message, bool yt, CommandParams cp)
         {
             var timer = new StopWatch();
 
@@ -76,24 +77,26 @@ namespace Witlesss.Commands
 
             var audio = hq ? " --audio-quality 0" : "";
 
-            var url = _url_prefix + id;
+            var url = yt ? _url_prefix + id : id;
 
             var output = $"{artist ?? (up ? "%(uploader)s" : "%(artist)s")} - {title ?? "%(title)s"} xd.%(ext)s";
-            var cmd_a = $"/C yt-dlp --no-mtime -f 251 -k -x --audio-format mp3{audio} {url} -o \"{output}\"";
-            var cmd_v = xt ? $"/C yt-dlp --no-mtime -f \"bv*[height<=720][filesize<15M]\" -k {url} -o \"video.%(ext)s\"" : null;
+            var cmd_a = $"/C yt-dlp --no-mtime {(yt ? "-f 251 -k " : xt || aa ? "" : "--write-thumbnail ")}-x --audio-format mp3{audio} {url} -o \"{output}\"";
+            var cmd_v = xt ? $"/C yt-dlp --no-mtime -f \"bv*{(yt ? "[height<=720][filesize<15M]" : "")}\" -k {url} -o \"video.%(ext)s\"" : null;
 
             var dir = $"{TEMP_FOLDER}/{DateTime.Now.Ticks}";
             Directory.CreateDirectory(dir);
 
             var thumb = $"{dir}/thumb.jpg";
             var task_a =      RunCMD(cmd_a, dir);
-            var task_v = xt ? RunCMD(cmd_v, dir) : aa ? Bot.DownloadFile(file, thumb, cp.Chat) : Task.Run(() => thumb = YouTubePreviewFetcher.DownloadPreview(id, dir).Result);
+            var task_v = xt ? RunCMD(cmd_v, dir) : aa ? Bot.DownloadFile(file, thumb, cp.Chat) : yt ? GetGoodYouTubeThumbnail() : Task.CompletedTask;
             await Task.WhenAll(task_a, task_v);
+            
+            Task GetGoodYouTubeThumbnail() => Task.Run(() => thumb = YouTubePreviewFetcher.DownloadPreview(id, dir).Result);
 
             var resize = cs || aa || thumb.Contains("maxres");
 
             var di = new DirectoryInfo(dir);
-            var thumb_source = xt ? di.GetFiles("video.*"  )[0].FullName : thumb;
+            var thumb_source = xt ? di.GetFiles("video.*"  )[0].FullName : yt || aa ? thumb : di.GetFiles("*.jpg")[0].FullName;
             var audio_file   =      di.GetFiles(  "*xd.mp3")[0].FullName;
 
             var meta = _name.Match(Path.GetFileName(audio_file));

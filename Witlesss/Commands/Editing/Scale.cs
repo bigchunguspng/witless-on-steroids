fda3fabfd6664fdd2b6208d186c9cbf3831141da
bigchungus <70759405+bigchunguspng@users.noreply.github.com>
2024-01-17 11:44:49 +0200

    /scalex 2

--- Witlesss/Commands/Editing/Scale.cs
+++ Witlesss/Commands/Editing/Scale.cs
@@ -8,6 +8,8 @@ namespace Witlesss.Commands.Editing
     {
         private const string _filename = "scale_fap_club.mp4";
 
+        private readonly Regex _number = new(@"^\d+(\.\d+)*$");
+
         public override void Run()
         {
             if (NoVideo()) return;
@@ -15,6 +17,13 @@ namespace Witlesss.Commands.Editing
             if (Text.Contains(' '))
             {
                 var args = Text.Split(' ').Skip(1).Take(2).ToArray();
+
+                var cmd = Text.ToLower();
+                if (cmd.Contains("scales") || cmd.Contains("scalex"))
+                {
+                    if (args.Length > 0 && _number.IsMatch(args[0])) args[0] = args[0] + "*w";
+                    if (args.Length > 1 && _number.IsMatch(args[1])) args[1] = args[1] + "*h";
+                }
 
                 for (var i = 0; i < args.Length; i++)
                 {

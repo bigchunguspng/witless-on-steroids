ae0e200101f41af01b5b4c5aca9675f7deb656e4
bigchungus <70759405+bigchunguspng@users.noreply.github.com>
2024-02-29 15:55:53 +0200

    #

--- Witlesss/Services/Memes/IFunnyApp.cs
+++ Witlesss/Services/Memes/IFunnyApp.cs
@@ -8,13 +8,13 @@ using Witlesss.MediaTools;
 using static System.Drawing.StringAlignment;
 
 namespace Witlesss.Services.Memes; // ReSharper disable InconsistentNaming
+
 public class IFunnyApp
 {
     public static bool UseSegoe, UseLeftAlignment, ThinCard, UltraThinCard, WrapText = true;
-    public static bool PickColor, ForceCenter, UseGivenColor, BackInBlack, BlurImage;
+    public static bool UseGivenColor, PickColor, ForceCenter, BackInBlack, BlurImage;
+    public static Color   GivenColor;
     public static int CropPercent = 100, MinFontSize = 10, DefFontSize = 36;
-    public static Color GivenColor;
-    public static readonly ExtraFonts ExtraFonts = new("top");
 
     private Color Background;
     private SolidBrush TextColor;
@@ -22,28 +22,32 @@ public class IFunnyApp
     private static readonly SolidBrush _transparent = new(Color.Transparent);
 
     private readonly EmojiTool _emojer = new() { MemeType = MemeType.Top };
+    public  static readonly ExtraFonts ExtraFonts = new("top");
     private static readonly Regex CAPS = new(@"[A-ZА-Я0-9bdfhkltбф]");
 
-    private int _w, _h, _t, _full, _ww, _crop_offset;
-    private float _lm, _caps_fix;
-    private bool _extra_high;
+    private int _w, _h; // <-- of the image
+    private int _cardHeight, _fullHeight, _cropOffset, _textWidth;
+    private float _marginLeft;
+    private float _capsFix; // <-- additional height offset to better center uppercase text
+    private bool _extraHigh;
     private SizeF _measure;
 
     public static float FontSize => MathF.Round(_sans.Size, 2);
 
-    public Point     Location => new(0, _t);
-    public Rectangle Cropping => new(0, _crop_offset, _w, _h);
+    public Point     Location => new(0, _cardHeight);
+    public Rectangle Cropping => new(0, _cropOffset, _w, _h);
 
+    private static Font _sans;
     private static FontFamily FontFamily => UseSegoe ? SegoeBlack : ExtraFonts.GetOtherFont("ft");
     private static FontFamily SegoeBlack = new("Segoe UI Black");
-    private static Font _sans;
+
     private static StringFormat Format => UseLeftAlignment ? _formatL : _formatC;
     private static readonly StringFormat _formatL = new() { Alignment = Near,   Trimming = StringTrimming.Word, LineAlignment = Far };
     private static readonly StringFormat _formatC = new() { Alignment = Center, Trimming = StringTrimming.Word, LineAlignment = Center };
 
     private void ResizeFont(float size) => _sans = new(FontFamily, size);
 
-    private int  StartingFontSize() => Math.Max(Math.Max(DefFontSize, _t / 5 * DefFontSize / 36), MinFontSize);
+    private int  StartingFontSize() => Math.Max(Math.Max(DefFontSize, _cardHeight / 5 * DefFontSize / 36), MinFontSize);
 
     private void SetFontToDefault() => ResizeFont(StartingFontSize());
     private void DecreaseFontSize() => ResizeFont(_sans.Size * 0.8f);
@@ -54,18 +58,20 @@ public class IFunnyApp
         var image = GetImage(path);
         var funny = DrawText(text);
 
-        return JpegCoder.SaveImage(Combine(image, funny), PngJpg.Replace(path, "-Top.jpg"));
+        var meme = Combine(image, funny);
+
+        return JpegCoder.SaveImage(meme, PngJpg.Replace(path, "-Top.jpg"));
     }
 
     private Image Combine(Image source, Image caption)
     {
-        var meme = new Bitmap(_w, _full);
+        var meme = new Bitmap(_w, _fullHeight);
         using var g = Graphics.FromImage(meme);
 
         if (Witlesss.Memes.Sticker) g.Clear(BackInBlack ? Color.Black : Background);
         
         g.CompositingMode = CompositingMode.SourceOver;
-        g.DrawImage(source,  new Point(0, _t - _crop_offset));
+        g.DrawImage(source,  new Point(0, _cardHeight - _cropOffset));
         g.DrawImage(caption, new Point(0, 0));
 
         return meme;
@@ -81,11 +87,9 @@ public class IFunnyApp
         AdjustProportions(textM);
         AdjustTextPosition(text);
 
-        var top = funny ? _t * 2 : _t;
+        var cardHeight = funny ? _cardHeight * 2 : _cardHeight;
+        var image = new Bitmap(_w, cardHeight);
 
-        var area = new RectangleF(_lm, _caps_fix, _ww, top);
-
-        var image = new Bitmap(_w, top);
         using var graphics = Graphics.FromImage(image);
         
         graphics.Clear(Background);
@@ -95,42 +99,44 @@ public class IFunnyApp
         graphics.PixelOffsetMode    = PixelOffsetMode.HighQuality;
         graphics.TextRenderingHint  = TextRenderingHint.AntiAlias;
 
+        var area = new RectangleF(_marginLeft, _capsFix, _textWidth, cardHeight);
+
         if (funny)
         {
             var p = new TextParams(62, EmojiSize, _sans, TextColor, area, Format);
             var h = (int)graphics.MeasureString(textM, _sans, area.Size, Format, out _, out var lines).Height;
             var l = _emojer.DrawTextAndEmoji(graphics, text, emoji, p, InitialMargin(h), Spacing);
-            var d = _t - h;
+            var d = _cardHeight - h;
             SetCardHeight(h * l / lines + d);
 
             graphics.CompositingMode = CompositingMode.SourceCopy;
-            graphics.FillRectangle(_transparent, 0, _t, _w, top - _t);
+            graphics.FillRectangle(_transparent, 0, _cardHeight, _w, cardHeight - _cardHeight);
         }
         else graphics.DrawString(text, _sans, TextColor, area, Format);
 
         return image;
     }
     
-    private int InitialMargin(int h) => UseLeftAlignment && !_extra_high ? _t - h : (_t - h) / 2;
+    private int InitialMargin(int h) => UseLeftAlignment && !_extraHigh ? _cardHeight - h : (_cardHeight - h) / 2;
     private int Spacing   => (int)(_sans.Size * 1.6);
     private int EmojiSize => (int)(_sans.Size * 1.5);
 
     private void AdjustProportions(string text)
     {
-        _extra_high = false;
+        _extraHigh = false;
 
         using var g = Graphics.FromHwnd(IntPtr.Zero);
 
         if (UseLeftAlignment)
         {
-            _lm = Math.Min(_sans.Size / 3, 5);
-            _ww = _w - (int)(2 * _lm);
+            _marginLeft = Math.Min(_sans.Size / 3, 5);
+            _textWidth = _w - (int)(2 * _marginLeft);
         }
 
-        var area = new SizeF(WrapText ? _ww : _ww * 3, _h * 5);
+        var area = new SizeF(WrapText ? _textWidth : _textWidth * 3, _h * 5);
 
         MeasureString();
-        while (_measure.Height > _t || _measure.Width > _ww) // fixes "text is too big"
+        while (_measure.Height > _cardHeight || _measure.Width > _textWidth) // fixes "text is too big"
         {
             DecreaseFontSize();
             MeasureString();
@@ -148,23 +154,23 @@ public class IFunnyApp
         void SetCardHeightXD(float x)
         {
             SetCardHeight((int)x);
-            _extra_high = true;
+            _extraHigh = true;
         }
         
         if (text.Count(c => c == '\n') > 2) // fixes "text is too small"
         {
-            var ms = g.MeasureString(text, _sans, new SizeF(_ww, _t));
+            var ms = g.MeasureString(text, _sans, new SizeF(_textWidth, _cardHeight));
             if (ms.Width < _w * 0.9)
             {
                 var k = 0.9f * _w / ms.Width;
                 ResizeFont(_sans.Size * k);
                 _measure = _measure * k;
 
-                if (_measure.Height > _t) SetCardHeightXD(_measure.Height + (_w - _measure.Width) / 2);
+                if (_measure.Height > _cardHeight) SetCardHeightXD(_measure.Height + (_w - _measure.Width) / 2);
             }
         }
         
-        if (ThinCard && _measure.Height < 0.95 * _t)
+        if (ThinCard && _measure.Height < 0.95 * _cardHeight)
         {
             var extraHeight = UltraThinCard ? _sans.Size * -0.1 : Math.Max(_sans.Size, 8);
             SetCardHeight((int)(_measure.Height + extraHeight));
@@ -173,9 +179,16 @@ public class IFunnyApp
 
     private void AdjustTextPosition(string s)
     {
-        var b = !UseLeftAlignment && CAPS.Matches(s).Count + EmojiRegex.Matches(s).Sum(m => m.Length) * 3 > s.Length / 5;
+        var fix = !UseLeftAlignment && TextIsUppercaseEnough(s);
 
-        _caps_fix = b ? _sans.Size * 0.0875f : 0;
+        _capsFix = fix ? _sans.Size * 0.0875f : 0;
+    }
+    
+    private bool TextIsUppercaseEnough(string s)
+    {
+        var caps  = CAPS.Matches(s).Count;
+        var emoji = EmojiRegex.Matches(s).Sum(m => m.Length);
+        return caps + 3 * emoji > s.Length / 5;
     }
 
     public void SetUp(Size size)
@@ -183,19 +196,19 @@ public class IFunnyApp
         _w = size.Width;
         _h = FF_Extensions.ToEven(size.Height * Math.Abs(CropPercent) / 100);
 
-        _crop_offset = CropPercent < 0 ? (size.Height - _h) / 2 : size.Height - _h;
+        _cropOffset = CropPercent < 0 ? (size.Height - _h) / 2 : size.Height - _h;
 
         SetCardHeight(_w > _h ? _w / _h > 7 ? _w / 7 : _h / 2 : _w / 2);
 
-        _lm = 0;
-        _ww = _w;
+        _marginLeft = 0;
+        _textWidth = _w;
 
         SetFontToDefault();
     }
     private void SetCardHeight(int x)
     {
-        _t = FF_Extensions.ToEven(x);
-        _full = _h + _t;
+        _cardHeight = FF_Extensions.ToEven(x);
+        _fullHeight = _h + _cardHeight;
     }
     private Image GetImage(string path)
     {
@@ -260,7 +273,7 @@ public class IFunnyApp
 
         Color AverageColorOnOffset(int x)
         {
-            var avg = AverageColor(image, new Rectangle(x, _crop_offset, 5, 5));
+            var avg = AverageColor(image, new Rectangle(x, _cropOffset, 5, 5));
             return BackInBlack ? avg : PutOver(Color.White, avg);
         }
     }

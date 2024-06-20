bec9d6098bb3264feda874a68f9b9ecfec5062a4
bigchungus <70759405+bigchunguspng@users.noreply.github.com>
2024-06-20 11:39:11 +0300

    when the new db schema saves 2 times faster but loads 2 times slower:

--- Witlesss/Generation/TransitionTableConverter.cs
+++ Witlesss/Generation/TransitionTableConverter.cs
@@ -1,39 +1,58 @@
 ﻿using System;
+using System.Collections.Generic;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace Witlesss.Generation;
 
-public class TransitionTableConverter : JsonConverter<TransitionTable>
+public class TransitionTableConverter : JsonConverter<Dictionary<int, TransitionTable>>
 {
-    public override void WriteJson(JsonWriter writer, TransitionTable value, JsonSerializer serializer)
+    public override void WriteJson(JsonWriter writer, Dictionary<int, TransitionTable> value, JsonSerializer serializer)
     {
         writer.WriteStartObject();
 
-        foreach (var transition in value)
+        foreach (var pair in value)
         {
-            writer.WritePropertyName(Base64Encoder.ToString(transition.WordID));
-            writer.WriteValue(transition.Chance);
+            writer.WritePropertyName(Base64Encoder.ToString(pair.Key));
+            writer.WriteStartObject();
+
+            foreach (var transition in pair.Value)
+            {
+                writer.WritePropertyName(Base64Encoder.ToString(transition.WordID));
+                writer.WriteValue(transition.Chance);
+            }
+
+            writer.WriteEndObject();
         }
 
         writer.WriteEndObject();
     }
 
-    public override TransitionTable ReadJson
+    public override Dictionary<int, TransitionTable> ReadJson
     (
-        JsonReader reader, Type type, TransitionTable value, bool hasvalue, JsonSerializer serializer
+        JsonReader reader, Type type, Dictionary<int, TransitionTable> value, bool hasvalue, JsonSerializer serializer
     )
     {
         var jo = JObject.Load(reader);
-        var transitions = new TransitionTable();
+        var dictionary = new Dictionary<int, TransitionTable>(jo.Count);
 
         foreach (var property in jo.Properties())
         {
-            var wordID = Base64Encoder.ToInt(property.Name);
-            var chance = property.Value.ToObject<float>();
-            transitions.Add(new Transition(wordID, chance));
+            var tableID = Base64Encoder.ToInt(property.Name);
+
+            var transitions = new TransitionTable();
+            var transitionsJson = (JObject)property.Value;
+
+            foreach (var transition in transitionsJson)
+            {
+                var wordID = Base64Encoder.ToInt(transition.Key);
+                var chance = transition.Value.ToObject<float>();
+                transitions.Add(new Transition(wordID, chance));
+            }
+
+            dictionary.Add(tableID, transitions);
         }
 
-        return transitions;
+        return dictionary;
     }
 }

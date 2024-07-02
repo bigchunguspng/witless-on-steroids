ba1e51370aec17ec229c8ad6c02b2b0ad5db3a28
bigchungus <70759405+bigchunguspng@users.noreply.github.com>
2024-07-02 10:37:08 +0300

    proper text distribution

--- Witlesss/Backrooms/Helpers/TextMeasuring.cs
+++ Witlesss/Backrooms/Helpers/TextMeasuring.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 using SixLabors.Fonts;
 using SixLabors.ImageSharp;
+using Size = SixLabors.ImageSharp.Size;
 
 namespace Witlesss.Backrooms.Helpers;
 
@@ -112,7 +114,7 @@ public static class TextMeasuring
     /// <summary>
     /// Replace all emoji in the text with "👌" before calling this method!
     /// </summary>
-    public static List<TextChunk> MeasureTextSuperCool
+    public static LinkedList<TextChunk> MeasureTextSuperCool
     (
         string text, TextOptions options, float emojiSize, int start = 0, bool cloneOptions = true
     )
@@ -131,7 +133,7 @@ public static class TextMeasuring
 
         TextMeasurer.TryMeasureCharacterAdvances(text, ops, out var advances);
 
-        var list = new List<TextChunk>();
+        var list = new LinkedList<TextChunk>();
 
         var type = CharType.Text;
         var length = 0;
@@ -145,20 +147,20 @@ public static class TextMeasuring
                 0x1F44C => CharType.Emoji,
                 _       => CharType.Text
             };
-            Append(advance, chunkType);
+            MoveForward(advance, chunkType);
         }
 
-        list.Add(new TextChunk(start, length, width, type));
+        list.AddLast(new TextChunk(start, length, width, type));
 
         return list;
 
         // ==
 
-        void Append(GlyphBounds advance, CharType chunkType)
+        void MoveForward(GlyphBounds advance, CharType chunkType)
         {
             if (type != chunkType || type == CharType.Emoji)
             {
-                list.Add(new TextChunk(start, length, width, type));
+                list.AddLast(new TextChunk(start, length, width, type));
                 type = chunkType;
                 start += length;
                 length = 0;
@@ -172,25 +174,105 @@ public static class TextMeasuring
         }
     }
 
-    private static List<TextChunk> MeasureTextSuperCoolMultiline
+    private static LinkedList<TextChunk> MeasureTextSuperCoolMultiline
     (
         string text, TextOptions options, float emojiSize
     )
     {
-        var list = new List<TextChunk>();
+        var list = new LinkedList<TextChunk>();
         var lines = text.Split('\n');
         for (var i = 0; i < lines.Length; i++)
         {
-            list.AddRange(MeasureTextSuperCool(lines[i], options, emojiSize, GetStart(), cloneOptions: false));
+            var chunks = MeasureTextSuperCool(lines[i], options, emojiSize, GetStart(), cloneOptions: false);
+            foreach (var chunk in chunks) list.AddLast(chunk);
+
             if (i + 1 < lines.Length)
             {
-                list.Add(new TextChunk(GetStart(), 1, 0F, CharType.LineBreak));
+                list.AddLast(new TextChunk(GetStart(), 1, 0F, CharType.LineBreak));
             }
         }
 
         return list;
 
         int GetStart() => list.LastOrDefault().GetNextChunkStart();
+    }
+
+    public static void DistributeText(LinkedList<TextChunk> chunks, int lines)
+    {
+        var widthTotal = chunks.Sum(x => x.Width);
+        var averageLineWidth = widthTotal / lines;
+        var currentLineWidth = 0F;
+        var linesFilled = 1;
+
+        var chunk = chunks.First!;
+        while (linesFilled < lines && chunk.Next is not null)
+        {
+            var limit = currentLineWidth + chunk.Value.Width;
+            if (limit >= averageLineWidth)
+            {
+                var lineBreak = new TextChunk(0, 0, 0F, CharType.LineBreak);
+
+                if (chunk.Value.Type == CharType.Spaces)
+                {
+                    chunk.Value = lineBreak;
+                    currentLineWidth = 0F;
+                }
+                else
+                {
+                    var breakBefore = averageLineWidth - currentLineWidth < limit - averageLineWidth;
+                    if (breakBefore)
+                    {
+                        if (chunk.Previous?.Value.Type == CharType.Spaces)
+                            chunk.Previous.Value = lineBreak;
+                        else
+                            chunks.AddBefore(chunk, lineBreak);
+                        currentLineWidth = chunk.Value.Width;
+                    }
+                    else
+                    {
+                        if (chunk.Next.Value.Type == CharType.Spaces)
+                            chunk.Next.Value = lineBreak;
+                        else
+                            chunks.AddAfter(chunk, lineBreak);
+                        currentLineWidth = 0F;
+                    }
+                }
+
+                linesFilled++;
+
+                if (lines > 2 && linesFilled == lines - 1)
+                {
+                    averageLineWidth = 0F;
+                    var node = chunks.FindLast(lineBreak)!.Next;
+                    while (node is not null)
+                    {
+                        averageLineWidth += node.Value.Width;
+                        node = node.Next;
+                    }
+
+                    averageLineWidth /= 2;
+                }
+            }
+            else
+            {
+                currentLineWidth = limit;
+            }
+
+            chunk = chunk.Next;
+        }
+    }
+
+    public static string FillWith(this LinkedList<TextChunk> chunks, string text)
+    {
+        var sb = new StringBuilder();
+        foreach (var chunk in chunks)
+        {
+            var n = chunk.Type == CharType.LineBreak;
+            if (n) sb.Append('\n');
+            else   sb.Append(text.Substring(chunk.Start, chunk.Length));
+        }
+
+        return sb.ToString();
     }
 }
 

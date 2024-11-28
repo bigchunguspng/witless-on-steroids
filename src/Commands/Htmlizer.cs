0dadfb561d3b5cab1c4e7eff7f475fd4f3b5093a
bigchungus <70759405+bigchunguspng@users.noreply.github.com>
2024-11-28 16:55:07 +0200

    #

--- src/Commands/Htmlizer.cs
+++ src/Commands/Htmlizer.cs
@@ -15,9 +15,10 @@ public class Htmlizer : SyncCommand
 
     protected override void Run()
     {
+        var htmlize = _mode is Mode.ToHtml;
         var message = Message.ReplyToMessage ?? Message;
 
-        var text = _mode is Mode.ToHtml ? HtmlText.Escape(message.ToHtml()) : message.GetTextOrCaption();
+        var text = htmlize ? HtmlText.Escape(message.ToHtml()) : message.GetTextOrCaption();
         if (text is null)
         {
             Bot.SendSticker(Chat, InputFile.FromFileId(LOL.PickAny()));
@@ -26,8 +27,8 @@ public class Htmlizer : SyncCommand
         {
             if (message == Message) text = text.SplitN(2)[1];
 
-            Bot.SendMessage(Chat, _mode is Mode.ToHtml ? $"<pre>{text}</pre>" : text);
-            Log($"{Title} >> {(_mode is Mode.ToHtml ? "<HTML/>" : "<TEXT/>")}");
+            Bot.SendMessage(Chat, htmlize ? $"<pre>{text}</pre>" : text);
+            Log($"{Title} >> {(htmlize ? "<HTML/>" : "<TEXT/>")}");
         }
     }
 

59b2f189b1dd2aa2d939576e4e8f4dc8c33bc79e
bigchungus <70759405+bigchunguspng@users.noreply.github.com>
2025-01-16 17:25:34 +0200

    Jarvis, update yt-dlp

--- src/Commands/Routing/CommandRouter.cs
+++ src/Commands/Routing/CommandRouter.cs
@@ -51,6 +51,7 @@ namespace Witlesss.Commands.Routing
                 .Register("peg"    , () => new FFMpeg())
                 .Register("w"      , () => new BrowseReddit())
                 .Register("link"   , () => new GetRedditLink())
+                .Register("run"    , () => new RunProcess())
                 .Register("kill"   , () => new KillProcess())
                 .Register("piece"  , () => _piece)
                 .Register("apeg"   , () => _apeg)

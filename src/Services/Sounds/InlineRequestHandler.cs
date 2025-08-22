0108396cfa39dff54d56424dd57291a0092e0ee2
bigchungus <70759405+bigchunguspng@users.noreply.github.com>
2025-08-22 16:18:23 +0300

    @bot (g) -> @bot (a)

--- src/Services/Sounds/InlineRequestHandler.cs
+++ src/Services/Sounds/InlineRequestHandler.cs
@@ -7,7 +7,7 @@ namespace Witlesss.Services.Sounds;
 
 public class InlineRequestHandler
 {
-    private readonly Regex _gif_mode = new(@"^[g!*.@гж](?=[\s]|$)");
+    private readonly Regex _sound_mode = new(@"^[as!*.@аз](?=[\s]|$)");
     private readonly Regex _caption  = new(@"^(.*?)\|\s?(.*)$");
 
     // @bot [g!*.@гж] query[|caption]
@@ -25,42 +25,42 @@ public class InlineRequestHandler
             caption = captionMatch.ExtractGroup(2, s => s);
         }
 
-        var gif_mode = !string.IsNullOrWhiteSpace(query) && _gif_mode.IsMatch(query);
-        if (gif_mode)
+        var sound_mode = !string.IsNullOrWhiteSpace(query) && _sound_mode.IsMatch(query);
+        if (sound_mode)
         {
             query = query!.Length < 3
                 ? null
                 : query.Substring(2);
         }
 
-        var results = GetResults(gif_mode, query, caption);
+        var results = GetResults(sound_mode, query, caption);
 
         await Bot.Instance.Client.AnswerInlineQuery(inline.Id, results.Take(50));
 
         var title = inline.From.GetFullNameTruncated();
-        var mode = gif_mode ? "g" : "a";
+        var mode = sound_mode ? "a" : "g";
         var   query_log = string.IsNullOrWhiteSpace(query)   ? "[empty]"    :       query;
         var caption_log = string.IsNullOrWhiteSpace(caption) ? string.Empty : $" | {caption}";
         Log($"[@inline-{mode}] {title} >> {query_log}{caption_log}");
     }
 
-    private List<InlineQueryResult> GetResults(bool gif_mode, string? query, string? caption)
+    private List<InlineQueryResult> GetResults(bool sound_mode, string? query, string? caption)
     {
         var results = new List<InlineQueryResult>();
 
-        var files = gif_mode
-            ? GIF_DB .Instance.Search(query) 
-            : SoundDB.Instance.Search(query);
+        var files = sound_mode
+            ? SoundDB.Instance.Search(query) 
+            : GIF_DB .Instance.Search(query);
 
-        if (gif_mode)
+        if (sound_mode)
             foreach (var file in files)
-                results.Add(new InlineQueryResultCachedGif(file.Id, file.FileId)
+                results.Add(new InlineQueryResultCachedVoice(file.Id, file.FileId, file.GetTitle())
                 {
                     Caption = caption, ParseMode = ParseMode.Html
                 });
         else
             foreach (var file in files)
-                results.Add(new InlineQueryResultCachedVoice(file.Id, file.FileId, file.GetTitle())
+                results.Add(new InlineQueryResultCachedGif(file.Id, file.FileId)
                 {
                     Caption = caption, ParseMode = ParseMode.Html
                 });

b1e66019a4eaa718787e0dd04b7b9ca90c6ea150
bigchungus <70759405+bigchunguspng@users.noreply.github.com>
2025-08-27 13:19:49 +0300

    #

--- src/PF_Bot/Program.cs
+++ src/PF_Bot/Program.cs
@@ -1,12 +1,5 @@
-﻿using System.Diagnostics;
-using PF_Bot.State;
-using PF_Bot.State.Generation;
+﻿using PF_Bot.State;
 using PF_Bot.Telegram;
-using PF_Bot.Tools_Legacy.Technical;
-using PF_Tools.Copypaster_Legacy;
-using PF_Tools.Copypaster_Legacy.Pack;
-using PF_Tools.Copypaster.Extensions;
-using PF_Tools.Copypaster.Helpers;
 
 namespace PF_Bot
 {

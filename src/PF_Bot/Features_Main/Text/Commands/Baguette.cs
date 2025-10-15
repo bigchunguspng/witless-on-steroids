5c3a4209772b4bbe33c067eba04b8c4f221d5127
bigchungus <70759405+bigchunguspng@users.noreply.github.com>
2025-10-15 12:06:31 +0300

    #@#

--- src/PF_Bot/Features_Main/Text/Commands/Baguette.cs
+++ src/PF_Bot/Features_Main/Text/Commands/Baguette.cs
@@ -1,45 +1,56 @@
 ﻿using System.Text;
 using PF_Bot.Routing.Commands;
 
-namespace PF_Bot.Features_Main.Text.Commands
+namespace PF_Bot.Features_Main.Text.Commands;
+
+public class Baguette : CommandHandlerBlocking
 {
-    public class Baguette : CommandHandlerBlocking
+    protected override void Run()
     {
-        protected override void Run()
+        var length    = Options.MatchNumber().ExtractGroup(0, int.Parse, 3);
+        var greentext = Options.Contains("b");
+
+        var text = BuildText(length, greentext);
+
+        Bot.SendMessage(Origin, text, preview: true);
+        Log($"{Title} >> {(greentext ? ">GREENTEXT >" : "BUGURT #@#")}{length}");
+    }
+
+    private string BuildText(int length, bool greentext)
+    {
+        var sign = greentext ? '>' : '@';
+
+        var lines = new string[length];
+        lines[0] = Args ?? GenerateLine();
+        for (var i = 1; i < length; i++) lines[i] = GenerateLine();
+
+        var sb = new StringBuilder();
+        if (greentext)
         {
-            var length = Options.MatchNumber().ExtractGroup(0, int.Parse, 3);
-            var start = Args;
+            foreach (var line in lines) sb.Append('>').AppendLine(line);
 
-            var greentext = Options.Contains("b");
-            var sign = greentext ? '>' : '@';
-
-            var lines = new List<string>(length) { start ?? GenerateLine() };
-            for (var i = 1; i < length; i++) lines.Add(GenerateLine());
-
-            var sb = new StringBuilder();
-            if (greentext)
+            if (Fortune.IsOneIn(3)) // append "summary"
             {
-                foreach (var line in lines) sb.Append('>').AppendLine(line);
-
-                if (Fortune.IsOneIn(3))
-                {
-                    if (Fortune.IsOneIn(2)) sb.Append('\n').Append(GenerateLine());
-                    else sb.Append(GenerateLine().InLetterCase(LetterCase.Sentence));
-                }
+                _ = Fortune.IsOneIn(2)
+                    ? sb
+                        .Append('\n')
+                        .Append(GenerateLine())
+                    : sb
+                        .Append(GenerateLine().InLetterCase(LetterCase.Sentence));
             }
-            else
-            {
-                for (var i = 0; i < lines.Count; i++)
-                {
-                    sb.Append(lines[i].ToUpper());
-                    if (i + 1 < lines.Count) sb.Append("\n@\n");
-                }
-            }
-
-            Bot.SendMessage(Origin, sb.ToString(), preview: true);
-            Log($"{Title} >> {(greentext ? ">GREENTEXT >" : "BUGURT #@#")}{length}");
-
-            string GenerateLine() => Baka.Generate().Split(sign, 2)[0].Trim();
         }
+        else // #@#
+        {
+            var lastLineIndex = length - 1;
+            for (var i = 0; i < length; i++)
+            {
+                sb.Append(lines[i].ToUpper());
+                if (i < lastLineIndex) sb.Append("\n@\n");
+            }
+        }
+
+        return sb.ToString();
+
+        string GenerateLine() => Baka.Generate().Split(sign, 2)[0].Trim();
     }
 }

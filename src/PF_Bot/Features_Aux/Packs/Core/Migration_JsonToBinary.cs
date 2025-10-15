144ae775cf5e30ffe4228a49fde4f861dc5bb8d5
bigchungus <70759405+bigchunguspng@users.noreply.github.com>
2025-10-16 00:32:04 +0300

    /mg fix 💀💀💀 (very hot)

--- src/PF_Bot/Features_Aux/Packs/Core/Migration_JsonToBinary.cs
+++ src/PF_Bot/Features_Aux/Packs/Core/Migration_JsonToBinary.cs
@@ -14,20 +14,29 @@ public static class Migration_JsonToBinary
             Log($"Migrating {dir}");
             foreach (var file in dir.GetFiles("*.json", recursive: true))
             {
-                var sw = Stopwatch.StartNew();
+                try
+                {
+                    var sw = Stopwatch.StartNew();
 
-                var bakaV1 = JsonIO.LoadData<PF_Tools.Copypaster_Legacy.Pack.GenerationPack>(file);
-                var bakaV2 = new GenerationPack();
-                bakaV2.FuseMigrate(bakaV1);
+                    var bakaV1 = JsonIO.LoadData<PF_Tools.Copypaster_Legacy.Pack.GenerationPack>(file);
+                    var bakaV2 = new GenerationPack();
+                    bakaV2.FuseMigrate(bakaV1);
 
-                var name = Path.GetFileNameWithoutExtension(file).Replace("pack-", "");
-                var newFileName = $"{name}{Ext_Pack}";
-                var path = Path.Combine(dir, newFileName);
-                GenerationPackIO.Save(bakaV2, path);
+                    var name = Path.GetFileNameWithoutExtension(file).Replace("pack-", "");
+                    var newFileName = $"{name}{Ext_Pack}";
 
-                File.Move(file, dir_json.Combine(dir).EnsureDirectoryExist().Combine(Path.GetFileName(file)));
+                    var parent = Path.GetDirectoryName(file)!;
+                    var path = Path.Combine(parent, newFileName);
+                    GenerationPackIO.Save(bakaV2, path);
 
-                sw.Log($"Migrated {file}");
+                    File.Move(file, dir_json.Combine(parent).EnsureDirectoryExist().Combine(Path.GetFileName(file)));
+
+                    sw.Log($"Migrated {file}");
+                }
+                catch (Exception e)
+                {
+                    Print($"MIGRATION FAIL | {file} | {e.GetErrorMessage()}");
+                }
             }
         }
         Log("Migration complete!", color: LogColor.Lime);

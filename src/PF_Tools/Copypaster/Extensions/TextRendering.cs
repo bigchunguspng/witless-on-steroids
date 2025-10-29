538c46e373eacaa0e8b3ac728879a0cd0046e78e
bigchungus <70759405+bigchunguspng@users.noreply.github.com>
2025-10-29 18:23:55 +0200

    🔥

--- src/PF_Tools/Copypaster/Extensions/TextRendering.cs
+++ src/PF_Tools/Copypaster/Extensions/TextRendering.cs
@@ -7,10 +7,6 @@ public static class TextRendering
     private const string LINK = GenerationPack.S_REMOVED;
     private const string LINK_en = "[deleted]", LINK_ua = "[засекречено]", LINK_ru = "[ссылка удалена]";
 
-    private const string ADD   = "[+]", ADD_Spaced = "[+] ";
-    private static readonly Regex
-        _r_random = new(@"\[\*(\d{1,9})..(\d{1,9})\]", RegexOptions.Compiled);
-
     public static string RenderText(this GenerationPack DB, LinkedList<int> ids)
     {
         var words = new LinkedList<string>();
@@ -20,14 +16,6 @@ public static class TextRendering
             var word = DB.GetWord(id);
             if (word is not null)
             {
-                var match = _r_random.Match(word); // EXAMPLE: [*0..15928]
-                if (match.Success)
-                {
-                    var a = Convert.ToInt32(match.Groups[1].Value);
-                    var b = Convert.ToInt32(match.Groups[2].Value);
-                    word = Fortune.RandomInt(a, b).ToString();
-                }
-
                 words.AddLast(word);
             }
         }
@@ -43,11 +31,6 @@ public static class TextRendering
         {
             var replacement = text.IsMostlyCyrillic() ? text.LooksLikeUkrainian() ? LINK_ua : LINK_ru : LINK_en;
             text = text.Replace(LINK, replacement);
-        }
-
-        if (words.Any(w => w.EndsWith(ADD))) // "ab[+] cd" -> "abcd"
-        {
-            text = text.Replace(ADD_Spaced, "");
         }
 
         return text.ToRandomLetterCase();

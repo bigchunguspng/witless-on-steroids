bb8341c4a5c9e20cc7351bdcc03a9599bbf6c98e
bigchungus <70759405+bigchunguspng@users.noreply.github.com>
2026-02-10 21:11:03 +0200

    top 10 hidden bugs

--- src/PF_Tools/Backrooms/Extensions/Extensions_Time.cs
+++ src/PF_Tools/Backrooms/Extensions/Extensions_Time.cs
@@ -13,16 +13,13 @@ public static class Extensions_Time
 
     public static string ReadableTime
         (this TimeSpan t)
-        =>    t.TotalSeconds < 10 ? $@"{t:s\,fff'`'ff' s'}"
+        =>    t.TotalSeconds < 10 ? $@"{t:s\,fff}`{t.Microseconds/10:00} s"
             : t.TotalMinutes <  1 ? $@"{t:s\,fff' s'}"
-            : t.TotalMinutes <  2 ? $"{t:m' MIN 's' s'}"
-            : t.TotalMinutes <  5 ? $"{t:m' MINS 's' s'}"
+            : t.TotalMinutes <  5 ? $"{t:m':'ss' M:SS'}"
             : t.TotalHours   <  1 ? $"{t:m' MINS'}"
-            : t.TotalHours   <  2 ? $"{t:h' HOUR 'm' MINS'}"
-            : t.TotalHours   <  5 ? $"{t:h' HOURS 'm' MINS'}"
+            : t.TotalHours   <  5 ? $"{t:h':'mm' H:MM'}"
             : t.TotalDays    <  1 ? $"{t:h' HOURS'}"
-            : t.TotalDays    <  2 ? $"{t:d' DAY 'h' HOURS'}"
-            : t.TotalDays    <  5 ? $"{t:d' DAYS 'h' HOURS'}"
+            : t.TotalDays    <  2 ? $"{t:d' DAY'}"
             :                       $"{t:d' DAYS'}";
 
     public static bool HappenedWithinLast

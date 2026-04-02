fd2092e0cd28194a35279f78999a8991f6bf9da3
bigchungus <70759405+bigchunguspng@users.noreply.github.com>
2026-04-02 11:49:27 +0300

    #

--- src/PF_Tools/Backrooms/Extensions/Extensions_Bool.cs
+++ src/PF_Tools/Backrooms/Extensions/Extensions_Bool.cs
@@ -1,20 +1,26 @@
+using System.Runtime.CompilerServices;
+
 namespace PF_Tools.Backrooms.Extensions;
 
 public static class Extensions_Bool
 {
     /// Compares condition to false. <br/> JAPANESE 100
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static bool Janai
         (this bool condition) => condition == false;
-    
+
     /// Compares condition to false.
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static bool IsOff
         (this bool condition) => condition == false;
 
     /// Compares condition to false.
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static bool Failed
         (this bool condition) => condition == false;
 
     /// Compares <see cref="Match.Success"/> to false.
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static bool Failed
         (this Match match) => match.Success == false;
 }

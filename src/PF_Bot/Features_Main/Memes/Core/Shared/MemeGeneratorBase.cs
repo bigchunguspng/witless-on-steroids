8564b023e1bcb2dec3cf70e810c51dab2089cfe7
bigchungus <70759405+bigchunguspng@users.noreply.github.com>
2026-09-22 18:08:52 +0300

    #

--- src/PF_Bot/Features_Main/Memes/Core/Shared/MemeGeneratorBase.cs
+++ src/PF_Bot/Features_Main/Memes/Core/Shared/MemeGeneratorBase.cs
@@ -12,21 +12,13 @@ public abstract class MemeGeneratorBase
 
     protected async Task FetchImageSize(MemeRequest request)
     {
-        for (var i = 3; i > 0; i--) // <-- retry mechanism
-        {
-            try
-            {
-                var info = await Image.IdentifyAsync(request.SourcePath);
-                _sourceSizeOG = info.Size;
-                _sourceSizeAdjusted = AdjustImageSize(request);
-                i = 0;
-            }
-            catch
-            {
-                LogError($"[WARNING] FetchImageSize -> CAN'T OPEN FILE \"{request.SourcePath}\", WAITING…");
-                await request.SourcePath.WaitForFile(checkEvery_ms: 125);
-            }
-        }
+        var info = await WhenFileIsFree
+        (
+            request.SourcePath, nameof(FetchImageSize),
+            () => Image.IdentifyAsync(request.SourcePath)
+        );
+        _sourceSizeOG = info.Size;
+        _sourceSizeAdjusted = AdjustImageSize(request);
     }
 
     protected async Task FetchVideoSize(MemeRequest request)
@@ -46,11 +38,35 @@ public abstract class MemeGeneratorBase
 
     protected async Task<Image<Rgba32>> GetImage(string path)
     {
-        var image = await Image.LoadAsync<Rgba32>(path);
+        var image = await WhenFileIsFree
+        (
+            path, nameof(GetImage),
+            () => Image.LoadAsync<Rgba32>(path)
+        );
         var resize = _sourceSizeOG != _sourceSizeAdjusted;
         if (resize)
             image.Mutate(x => x.Resize(_sourceSizeAdjusted));
 
         return image;
+    }
+
+    private static async Task<T> WhenFileIsFree<T>
+        (FilePath path, string method, Func<Task<T>> task)
+    {
+        var i = 0;
+        while (true)
+        {
+            try
+            {
+                return await task();
+            }
+            catch
+            {
+                LogError($"[WARNING] {method} -> CAN'T OPEN FILE \"{path}\", WAITING…");
+                if (++i > 3) throw;
+
+                await path.WaitForFile(checkEvery_ms: 125);
+            }
+        }
     }
 }
